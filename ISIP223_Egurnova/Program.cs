9940cd4b61338155bca12aa529369b11aa03cf87
arincha123 <a_egurnova@inbox.ru>
2025-10-08 14:17:54 +0300

    начало работы

--- ISIP223_Egurnova/Program.cs
+++ ISIP223_Egurnova/Program.cs
@@ -5,7 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ISIP223_Egurnova
-{
+
+
+{ 
+    class Person
+    {
+        private string FIO;
+        private DateTime Birthday;
+        private string Gender;
+
+        public Person(string fio, DateTime birthday, string gender)
+        {
+            FIO = fio;
+            Birthday = birthday;
+            Gender = gender;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"ФИО: {FIO}\nДата рождения: {Birthday}\nПол: {Gender}");
+        }
+    }
+
+
+    class Student : Person
+    {
+        private int ID;
+        private int CourseNum;
+
+        public Student(int id, string fio, DateTime birthday, string gender, int CN)
+            : base(fio, birthday, gender)
+        {
+            ID = id;
+            CourseNum = CN;
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"ID: {ID}\nНомер курса: {CourseNum}");
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     internal class Program
     {
         static void Main(string[] args)
