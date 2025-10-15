a0cd09945dfd850cd3d597c467c66aaa182ad5a0
arincha123 <a_egurnova@inbox.ru>
2025-10-15 14:18:41 +0300

    Инициализация классов

--- ISIP223_Egurnova/Program.cs
+++ ISIP223_Egurnova/Program.cs
@@ -6,6 +6,45 @@ using System.Threading.Tasks;
 
 namespace ISIP223_Egurnova
 {
+    class Basa
+    {
+        public int HP { get; set; }
+        public int ATK { get; set; }
+        public int DEF { get; set; }
+        public string EQP { get; set; }
+        public string WEAP { get; set; }
+        public int Krit { get; set; }
+
+
+
+    }
+
+    class Enwmy
+    {
+        public int HP { get; set; }
+        public int ATK { get; set; }
+        public int DEF { get; set; }
+        public int Krit { get; set; }
+        public string TypeATK { get; set; }
+
+
+    }
+
+    class Goblin : Basa
+    {
+
+    }
+
+    class Skelet : Basa
+    {
+
+    }
+
+    class Mage : Basa
+    {
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
