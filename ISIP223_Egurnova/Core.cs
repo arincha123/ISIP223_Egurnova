b92a454345b10e9c715bb02ba94dcdc5429b98c2
arincha123 <a_egurnova@inbox.ru>
2025-10-29 11:27:32 +0300

    Подключена база данных

---
+++ ISIP223_Egurnova/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Egurnova
+{
+    internal class Core
+    {
+        public static auEntities Context = new auEntities();
+
+    }
+}
--- ISIP223_Egurnova/Program.cs
+++ ISIP223_Egurnova/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,61 +21,85 @@ namespace ISIP223_Egurnova
      
      */
 
-    public class Client
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int ID_car { get; set; }
-
-
-
-
-    }
-
-    public class Car
-    {
-        public int ID { get; set; }
-        public string Model { get; set; }
-        public string Mark { get; set; }
-        public int ID_detail { get; set; }
-
-
-
-
-
-    }
-
-    public class Detail
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public float Price { get; set; }
-
-
-
-
-
-    }
-
-
-    public class Sklad
-    {
-
-    }
-
-    public class Game
-    {
-
-    }
-
-
-
-
-
     internal class Program
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            game.Start();
+            
         }
     }
+
+
+    public class Game
+    {
+
+        private int balance { get; set; }
+        private int shtraf { get; set; }
+        private int obsluga { get; set; }
+        private int day {  get; set; }
+
+        private List<Client> tekuchclient;
+        private List<Car> tekuchcar;
+        private List<Detail> tekuchdetail;
+
+        Random Random = new Random();
+
+        public void Start()
+        {
+            balance = 3300;
+            shtraf = 300;
+            obsluga = 250;
+            day = 1;
+
+
+
+            /* Рандом
+            tekuchdetail = Core.Context.Detail.ToList();
+
+            Detail detail = tekuchdetail[Random.Next(tekuchdetail.Count)];
+
+            Console.WriteLine(detail.Name);*/
+
+
+
+
+        }
+
+        private void mainMenu()
+        {
+            Console.WriteLine($"=== АВТОМАСТЕРСКАЯ === === ДЕНЬ {day}");
+            Console.WriteLine($"=== БАЛАНС {balance}");
+            Console.WriteLine($"=== ---------------------------- ===");
+            Console.WriteLine($"1. Инвентаризация");
+            Console.WriteLine($"2. Заказать детали");
+            Console.WriteLine($"3. Принять клиента");
+            Console.WriteLine($" ");
+            Console.WriteLine($"0. Выйти из игры ");
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
